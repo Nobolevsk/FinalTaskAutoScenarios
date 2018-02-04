036e5434d8a6aab8db29d55eb74692746f59dd4e
Novruz <nobolevsk@gmail.com>
2018-02-04 22:38:21 +0300

    Final task Autotest

--- src/main/java/pages/Parameters.java
+++ src/main/java/pages/Parameters.java
@@ -1,15 +1,20 @@
 package pages;
 
+import org.apache.http.util.Asserts;
+import org.junit.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
+
 public class Parameters extends BaseClass {
 
     @FindBy(xpath = "//div[@class = 'layout__col i-bem exp-filters-new-design layout__col_search-filters_visible']")
     WebElement searchParameters;
+    @FindBy(xpath = "//div[contains(@class, 'n-snippet-list n-snippet-list_type_vertical')]")
+    WebElement checkSumElements;
 
     public Parameters(WebDriver driver){
         PageFactory.initElements(driver, this);
@@ -23,4 +28,17 @@ public class Parameters extends BaseClass {
         searchParameters.findElement(By.xpath("//button[contains(@class, 'button_action_n-filter-apply')]")).click();
 
     }
+
+    public void countElements() {
+        WebElement elements = checkSumElements.findElement(By.xpath("//div[contains(@class, 'n-snippet-card2 i-bem b-zone b-spy-visible b-spy-visible_js_inited')]"));
+        Assert.assertEquals(By.xpath
+                        ("//div[contains(@class, 'n-snippet-card2 i-bem b-zone b-spy-visible b-spy-visible_js_inited')]"),
+                12);
+
+        checkSumElements.findElement(By.xpath("//input[@id = 'header-search']")).sendKeys((CharSequence) elements);
+
+        checkSumElements.findElement(By.xpath("(//button[@type = 'submit'])[1]")).click();
+    }
+
+
 }
